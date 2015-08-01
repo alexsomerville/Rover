2420f79c87a6aa8e1c24d99cc6db1c399c1a6b7a
alexsomerville <alexsomerville2004@gmail.com>
2015-08-01 13:26:16 +0100

    Create logging.h

---
+++ logging.h
@@ -0,0 +1,19 @@
+#ifdef LOGGING
+
+#include <stdarg.h>
+
+void log(char* format, ...)
+{
+    char line[1024];
+    va_list args;
+    va_start(args, format);
+    vsnprintf(line, sizeof(line), format, args);
+    va_end(args);
+    Serial.print(line);
+}
+
+#else
+
+#define log(...)
+
+#endif
